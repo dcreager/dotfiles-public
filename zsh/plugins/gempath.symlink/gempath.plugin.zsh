76ab4631d1d4eaaef695fca563a9465d6324b2c0
Douglas Creager <douglas.creager@redjack.com>
2014-02-18 11:53:34 -0500

    Don't hard-code Ruby version in GEM_PATH

--- zsh/plugins/gempath.symlink/gempath.plugin.zsh
+++ zsh/plugins/gempath.symlink/gempath.plugin.zsh
@@ -1,6 +1,4 @@
-if [[ -d ~/.gem/ruby/1.9.1/bin ]]; then
-   path=(~/.gem/ruby/1.9.1/bin $path)
-fi
-if [[ -d ~/.gem/ruby/2.0.0/bin ]]; then
-   path=(~/.gem/ruby/2.0.0/bin $path)
+if which ruby >/dev/null 2>/dev/null; then
+    export GEM_HOME=$(ruby -e 'puts Gem.user_dir')
+    path=($GEM_HOME/bin $path)
 fi
